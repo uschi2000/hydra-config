8e1be7d8c476e87d65f7b167eef7f7ac4bc871a6
Robert Fink <robert.fink@helsing.ai>
2023-07-29 13:10:30 +0300

    Add config example with default list

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -42,11 +42,13 @@ def test_decorator_parses_config() -> None:
         assert config.name == "foo"
         assert config.answer == 42
         assert config.tags == ["a", "b"]
+        assert isinstance(config.tags, list)
 
-        assert config.inputs["some_local_input"] == LocalInput(Path("/data"))
-        assert config.inputs["some_remote_input"] == RemoteInput(
+        assert config.inputs["default_local_input"] == LocalInput(Path("/data"))
+        assert config.inputs["default_remote_input"] == RemoteInput(
             "https://foo/bar",
             Token(UUID("7a72f169-f8c3-4b3e-8041-021a62a2d87f"), "my_token"),
         )
+        assert config.inputs["extra_local_input"] == LocalInput(Path("/data_extra"))
 
     main()
