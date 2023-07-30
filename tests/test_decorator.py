061cd85bf21db5c942c199143ddc665dd85f81ab
Robert Fink <robert.fink@helsing.ai>
2023-07-30 14:36:45 +0300

    Input -> Dataset

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -8,19 +8,17 @@ from hydra_config.decorator import hydra_main2, Token
 
 
 @dataclass(frozen=True)
-class RemoteInput:
+class RemoteDataset:
     url: str
     token: Token
-    kind: Literal["RemoteInput"] = "RemoteInput"
 
 
 @dataclass(frozen=True)
-class LocalInput:
+class LocalDataset:
     path: Path
-    kind: Literal["LocalInput"] = "LocalInput"
 
 
-InputTypes = RemoteInput | LocalInput
+InputTypes = RemoteDataset | LocalDataset
 InputConfigurations = dict[str, InputTypes]
 
 
@@ -44,11 +42,11 @@ def test_decorator_parses_config() -> None:
         assert config.tags == ["a", "b"]
         assert isinstance(config.tags, list)
 
-        assert config.inputs["default_local_input"] == LocalInput(Path("/data"))
-        assert config.inputs["default_remote_input"] == RemoteInput(
+        assert config.inputs["default_local_dataset"] == LocalDataset(Path("/data"))
+        assert config.inputs["default_remote_dataset"] == RemoteDataset(
             "https://foo/bar",
             Token(UUID("7a72f169-f8c3-4b3e-8041-021a62a2d87f"), "my_token"),
         )
-        assert config.inputs["extra_local_input"] == LocalInput(Path("/data_extra"))
+        assert config.inputs["extra_local_dataset"] == LocalDataset(Path("/data_extra"))
 
     main()
